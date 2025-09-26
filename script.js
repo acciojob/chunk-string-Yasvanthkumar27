12f2bd673a1a66a2987d4ed5e8cc7af2317cebc6
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-09-26 13:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	if (!str.length || size <= 0) return [];
+    let result = [];
+    for (let i = 0; i < str.length; i += Number(size)) {
+        result.push(str.slice(i, i + Number(size)));
+    }
+    return result;
+
 }
 
 // Do not change the code below
